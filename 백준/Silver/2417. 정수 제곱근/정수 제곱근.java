661bd6f053c2ef0bb7543bad36d48ea00da387c0
junsu <90898067+developHeo515@users.noreply.github.com>
2024-09-29 01:17:27 +0900

    Update 정수 제곱근.java

--- 백준/Silver/2417. 정수 제곱근/정수 제곱근.java
+++ 백준/Silver/2417. 정수 제곱근/정수 제곱근.java
@@ -1,22 +1,32 @@
 //BOJ2417 정수 제곱근, 실버4
 //처음에는 이 문제와 이분탐색이 어떤 접점이 있는지 이해하기 어려웠음
+// 1번 방법 Math.sqrt
+// 2번 방법 이분 탐색 - 이걸로 품
 import java.io.*;
-import java.math.BigInteger;
-import java.util.*;
 
-public class Main {
+public class BOJ2417 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		long num = Long.parseLong(br.readLine());
-		long sqrt = (long) Math.sqrt(num);
+
+		long start = 0;
+		long end = num;
+		long ans = 0;
 		
-		if(sqrt * sqrt < num) {
-			sqrt++;
+		while(start <= end) {
+			long mid = (start+end) / 2;
+			if(num <= Math.pow(mid, 2)) {
+				ans = mid;
+				end = mid-1;
+			} else {
+				start = mid+1;
+			}
 		}
-		System.out.println(sqrt);
-
-
-	
+		
+		bw.write(ans+"\n");
+		bw.flush();
+		bw.close();
+		br.close();
 	}
 }
