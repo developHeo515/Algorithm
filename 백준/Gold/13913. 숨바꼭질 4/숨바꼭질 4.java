a12269d04737a38cba1b1124a8f5edef31055b22
junsu <90898067+developHeo515@users.noreply.github.com>
2024-10-07 02:33:38 +0900

    [Gold IV] Title: 숨바꼭질 4, Time: 316 ms, Memory: 35472 KB -BaekjoonHub

--- 백준/Gold/13913. 숨바꼭질 4/숨바꼭질 4.java
+++ 백준/Gold/13913. 숨바꼭질 4/숨바꼭질 4.java
@@ -1,18 +1,11 @@
 //BOJ13913 숨바꼭질4, 골드4
 //기존 숨바꼭질 문제에서 BFS 최단거리 경로를 저장하는 배열을 사용
 //경로 기록은 순서대로 구하기 위해 stack에 담았다가 pop하면된다.
+//최적화 완료
 import java.io.*;
 import java.util.*;
 
 public class Main {
-	static class P{
-		int location;
-		int cnt;
-		public P(int location, int cnt) {
-			this.location = location;
-			this.cnt = cnt;
-		}
-	}
 	static int[] point, parent;
 	static int N, K, time;
 	public static void main(String[] args) throws IOException {
@@ -48,55 +41,34 @@ public class Main {
 	}
 	static int bfs() {
 		if(N == K) return 0; // 0, 0일 때 예외 케이스 처리해줘야한다.
-		Queue<P> q = new LinkedList<>();
-		q.add(new P(N, 0));
+		
+		Queue<Integer> q = new LinkedList<>();
+		q.add(N);
+		point[N] = 1;
 		
 		while(!q.isEmpty()) {
-			P cur = q.poll();
-			int location = cur.location;
-			int cnt = cur.cnt;
+			int location = q.poll();
 			
 //			System.out.print(location  +  "-");
 			
-			if(location-1 >= 0) {
-				if(point[location-1] == 0 && location-1 != N) { //이동한 위치가 수빈이 원래 위치 아니라면 1초후 X-1로 이동
-					if(location-1 ==  K) { //동생위치 찾았을 때
-						time = cnt+1;
-						parent[location-1] = location;
-						return time;
-					}
-					point[location-1] = cnt+1;
-					q.add(new P(location-1, cnt+1));
-					parent[location-1] = location;
+			for(int i = 0; i < 3; i++) {
+				int next;
+				
+				if(i == 0) next = location - 1;
+				else if(i == 1) next = location + 1;
+				else next = location * 2;
+				
+				if(next < 0 || next > 100000) continue;
+				
+				if(point[next] == 0) {
+					q.add(next);
+					point[next] = point[location] + 1;
+					parent[next] = location;
 				}
 			}
-			
-			if(location+1 <= 100000) {
-				if(point[location+1] == 0 && location+1 != N) { //이동한 위치가 수빈이 원래 위치 아니라면 1초후 X+1로 이동
-					if(location+1 ==  K) { //동생위치 찾았을 때
-						time = cnt+1;
-						parent[location+1] = location;
-						return time;
-					}
-					point[location+1] = cnt+1;
-					q.add(new P(location+1, cnt+1));
-					parent[location+1] = location;
-				}
-			}
-			
-			if(location * 2 <= 100000) {
-				if(point[location*2] == 0 && location*2 != N) { //이동한 위치가 수빈이 원래 위치 아니라면 1초후 2*X로 이동
-					if(location*2 ==  K) { //동생위치 찾았을 때
-						time = cnt+1;
-						parent[location*2] = location;
-						return time;
-					}
-					point[location*2] = cnt +1;
-					q.add(new P(location*2, cnt+1));
-					parent[location*2] = location;
-				}
-			}
+
 		}
+		if(point[K] != 0) return point[K] - 1;
 		return -1;
 	}
 }
