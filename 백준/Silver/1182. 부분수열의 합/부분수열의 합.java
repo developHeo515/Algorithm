1002582345d45dba5c6acd7918124ee4930fa457
junsu <90898067+developHeo515@users.noreply.github.com>
2024-10-29 00:46:34 +0900

    [Silver II] Title: 부분수열의 합, Time: 132 ms, Memory: 15916 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.java
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.java
@@ -1,34 +1,40 @@
-import java.util.Scanner;
+//BOJ1182 부분수열의 합, 실버2
+import java.io.*;
+import java.util.*;
 
-class Main {
-    static int[] num;
-    private static int N; // 정수의 개수
-    private static int S; // 정수의 합
-    private static int answer = 0;
-
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        N = sc.nextInt();
-        S = sc.nextInt();
-        num = new int[N];
-        for (int i = 0; i < N; i++) {
-            num[i] = sc.nextInt();
-        }
-        dfs(0, 0);
-        if (S == 0) System.out.println(answer - 1); 
-        //S 0일때가 전체 합이 0 일때랑 겹치기 때문에 -1 을 해주어야 합니다.
-        //위의 사진에 공집합이 0이라고 볼 수 있습니다
-         else  System.out.println(answer);
-
-    }
-
-    private static void dfs(int depth, int sum) {
-        if (depth == N) {
-            if (sum == S) answer++;
-            return;
-        }
-        //tree를 생각해서 직접 그려보면 이해하기가 쉽습니다.
-        dfs(depth + 1, sum + num[depth]);
-        dfs(depth + 1, sum); 
-    }
+public class Main {
+	static int[] num;
+	static int N, S;
+	static int ans = 0;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		S = Integer.parseInt(st.nextToken());
+		
+		num = new int[N];
+		st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < N; i++) {
+			num[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		dfs(0, 0);
+		
+		if(S == 0) bw.write((ans - 1)+"\n"); //S가 0일때랑 겹치는 경우 때문에 -1로 해줌
+		else bw.write(ans+"\n");
+		
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+	static void dfs(int depth, int sum) {
+		if(depth == N) {
+			if(sum == S) ans++;
+			return;
+		}
+		
+		dfs(depth+1, sum + num[depth]);
+		dfs(depth+1, sum);
+	}
 }
