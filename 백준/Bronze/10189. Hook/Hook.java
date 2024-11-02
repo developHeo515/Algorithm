a55a364964a9334d9400ee070b8c186d10856caa
junsu <90898067+developHeo515@users.noreply.github.com>
2024-11-02 09:27:45 +0900

    [Bronze V] Title: Hook, Time: 96 ms, Memory: 13880 KB -BaekjoonHub

---
+++ 백준/Bronze/10189. Hook/Hook.java
@@ -0,0 +1,12 @@
+//BOJ10189 Hook 브5
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("#  # #### #### #  #");
+		System.out.println("#### #  # #  # # #");
+		System.out.println("#### #  # #  # # #");
+		System.out.println("#  # #### #### #  #");
+	}
+}
