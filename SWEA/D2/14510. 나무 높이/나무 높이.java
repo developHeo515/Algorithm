dd1b040e2d941bd2259571cd5820e704956e4f06
junsu <90898067+developHeo515@users.noreply.github.com>
2024-11-12 23:57:17 +0900

    [D2] Title: 나무 높이, Time: 107 ms, Memory: 18,900 KB -BaekjoonHub

--- SWEA/D2/14510. 나무 높이/나무 높이.java
+++ SWEA/D2/14510. 나무 높이/나무 높이.java
@@ -1,54 +1,64 @@
-//SWEA14510 나무 높이, D2 
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
+/**
+ * SWEA 14150. 나무 높이
+ * @category 그리디
+ */
 public class Solution {
-	static int ans;
-	static int N;
-	static int[] tree;
-	public static void main(String[] args) throws IOException {
+
+	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringTokenizer st;
 		
 		int T = Integer.parseInt(br.readLine());
-		for(int tc = 1; tc <= T; tc++) {
-			ans = 0;
-			N = Integer.parseInt(br.readLine());
-			tree = new int[N];
-			int max = 0;
-			int one = 0;
-			int two = 0;
+		for(int tc = 1; tc <= T; tc++)
+		{
+			int N = Integer.parseInt(br.readLine());
 			
 			st = new StringTokenizer(br.readLine());
-			for(int i = 0; i < N; i++) {
-				int num = Integer.parseInt(st.nextToken());
-				max = Math.max(max, num);
-				tree[i] = num;
-			}
 			
-			for(int i = 0; i < N; i++) {
-				one += (max - tree[i]) % 2;
-				two += (max - tree[i]) / 2;
-			}
-			// 짝수 날짜를 적절히 조정하여 균형을 맞추어주어야 최소 날짜가 나오게 됩니다. 
-			int temp = Math.max(two - one, 0) / 3;
-			one += temp * 2;
-			two -= temp;
-			int Min = Math.min(two, one);
-			
-			ans = Min * 2 
-					+ Math.max((one - Min) * 2 - 1, 0)
-					+(two - Min) / 2 * 3
-					+(two - Min) % 2 * 2;
-			
-			
-			bw.write("#" + tc + " " + ans + "\n");
-		}
+			int max = 0; //가장 큰 나무의 높이
+			int[] trees = new int[N];
+			for (int i = 0; i < N; i++) {
+				int t = Integer.parseInt(st.nextToken());
+				trees[i] = t;
 				
-		
-		bw.flush();
-		bw.close();
-		br.close();
+				max = Math.max(max, t);
+			}
+			
+			//나무가 자라야 할 높이에서 필요한 1, 2의 개 수 구하기
+			int even = 0, odd = 0;
+			for (int i = 0; i < N; i++) {
+				int diff = max - trees[i];
+				
+				if(diff == 0) continue;
+				
+				even += diff / 2;
+				odd += diff % 2;
+			}
+			
+			//2 -> 1로 변경
+			if(even > odd) {
+				while(Math.abs(even - odd) > 1) {
+					even--;
+					odd += 2;
+				}
+			}
+			
+			int result = 0;
+			if(odd > even) { //1이 많을 경우
+				result = odd * 2 - 1;
+				
+			} else if(even > odd) { //2가 많을 경우 
+				result = even * 2;
+				
+			} else { //1과 2의 숫자가 같을 경우
+				result = odd + even;
+			}
+			
+			System.out.println("#" + tc + " " + result);
+		}
 	}
 }
