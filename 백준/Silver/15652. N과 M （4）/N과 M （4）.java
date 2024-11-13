05afb86dde6752386f4fb74172dfb7b1fd42ad73
junsu <90898067+developHeo515@users.noreply.github.com>
2024-11-13 10:11:05 +0900

    [Silver III] Title: N과 M (4), Time: 180 ms, Memory: 19416 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -0,0 +1,42 @@
+//BOJ15652 N과 M(4)
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static int N, M;
+	static int[] arr, selected;
+	static boolean[] visited;
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	public static void main(String[] args) throws IOException{
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		arr = new int[N];
+		visited = new boolean[N];
+		selected = new int[M];
+		for(int i = 0; i < N; i++) {
+			arr[i] = i+1;
+		}
+		
+		func(0, 0);
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+	static void func(int depth, int start) throws IOException {
+		if(depth == M) {
+			for(int i = 0; i < M; i++) {
+				bw.write(selected[i] + " ");
+			}
+			bw.write("\n");
+			return;
+		}
+		
+		for(int i = start; i < N; i++) {
+			selected[depth] = arr[i];
+			func(depth+1, i);		
+		}
+	}
+}
