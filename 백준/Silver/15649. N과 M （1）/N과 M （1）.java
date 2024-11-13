a1df0bf3b3521c4c1fd049996bdfd4af263761dc
junsu <90898067+developHeo515@users.noreply.github.com>
2024-11-13 09:18:46 +0900

    [Silver III] Title: N과 M (1), Time: 1608 ms, Memory: 59320 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -1,51 +1,48 @@
+//BOJ15649 N과 M(1)
+//백트래킹 다시 풀어보기
 import java.io.*;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.*;
 
-
 public class Main {
-	static int[] arr;
-	static StringBuilder sb;
+	static int N, M;
+	static int[] arr, selected;
 	static boolean[] visited;
-	static int n;
-	static int m;
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
-        
-        sb = new StringBuilder();
-        visited = new boolean[n+1];
-        arr = new int[m];
-        
-        dfs(0);
-        
-        System.out.println(sb);
-    }
-    
-    static void dfs(int depth) {
-    	
-    	if(depth == m) {
-    		for(int n : arr) {
-    			sb.append(n).append(" ");
-    		}
-    		sb.append("\n");
-    		
-    		return;
-    	}
-    	
-    	for(int i=1; i<=n; i++) {
-    		if(!visited[i]) {
-    			visited[i] = true;
-    			
-    			arr[depth] = i;
-    			dfs(depth+1);
-    			
-    			visited[i] = false;
-    		}
-    	}
-    }
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		arr = new int[N];
+		visited = new boolean[N];
+		selected = new int[M];
+		for(int i = 0; i < N; i++) {
+			arr[i] = i+1;
+		}
+		
+		func(0);
+		
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+	static void func(int depth) {
+		if(depth == M) {
+			for(int i = 0; i < M; i++) {
+				System.out.print(selected[i] + " ");
+			}
+			System.out.println();
+			return;
+		}
+		
+		for(int i = 0; i < N; i++) {
+			if(!visited[i]) {
+				visited[i] = true;
+				selected[depth] = arr[i];
+				func(depth+1);		
+				visited[i] = false;
+			}
+		}
+	}
 }
