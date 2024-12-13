063e0ed46bb8677c7609558677fff122ab96106b
junsu <90898067+developHeo515@users.noreply.github.com>
2024-12-14 02:54:45 +0900

    Update 패션왕 신해빈.java

--- 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.java
+++ 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.java
@@ -1,4 +1,3 @@
-//BOJ9375 패션왕 신해빈, 실버3
 import java.io.*;
 import java.util.*;
 
@@ -16,16 +15,21 @@ public class Main {
 			for(int j = 0; j < N; j++) {
 				st = new StringTokenizer(br.readLine());
 				String clothes = st.nextToken();
-				String key = st.nextToken();
-				map.put(key, map.getOrDefault(key, 0) + 1);
+				String kind = st.nextToken();
+				
+//				map.put(kind, map.getOrDefault(kind, 0) + 1);
+				if(map.containsKey(kind)) { // 해당 옷이 해시맵에 있을 경우
+					map.put(kind, map.get(kind) + 1);
+				}else { //해당 옷이 해시맵에 없을 경우
+					map.put(kind, 1);
+				}
 			}
 			int cnt = 1;
-			for(Map.Entry<String, Integer> entry : map.entrySet()) {
-				cnt = cnt * (entry.getValue()+1);
+			for(int val : map.values()) {
+				cnt *= (val + 1);
 			}
-			System.out.println(cnt-1);
+			bw.write(cnt-1 + "\n"); //알몸인 상태를 제외해줘야해서 -1 해줌
 		}
-		
 		
 		bw.flush();
 		bw.close();
