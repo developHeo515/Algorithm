9b784b7006a049c00a382d225fa7f3a841b1a952
junsu <90898067+developHeo515@users.noreply.github.com>
2025-01-23 00:48:44 +0900

    [Bronze V] Title: 새싹, Time: 92 ms, Memory: 14008 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,16 @@
+//BOJ25083 새싹, 브5
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) {
+		String s = "         ,r'\"7\n" + 	
+				   "r`-_   ,'  ,/\n" + 		
+				   " \\. \". L_r'\n" + 		
+				   "   `~\\/\n" + 			
+				   "      |\n" + 			
+				   "      |";
+ 
+		System.out.print(s);
+	}
+}
