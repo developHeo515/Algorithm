37d9c707353ca420b094fad284db8bd0aeecfb66
junsu <90898067+developHeo515@users.noreply.github.com>
2025-02-26 04:27:34 +0900

    [Bronze V] Title: AI 선도대학, Time: 100 ms, Memory: 14292 KB -BaekjoonHub

---
+++ 백준/Bronze/32951. AI 선도대학/AI 선도대학.java
@@ -0,0 +1,15 @@
+//BOJ25372 성택이의 은밀한 비밀번호, 브5
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		System.out.println(N - 2024);
+		
+	}
+}
