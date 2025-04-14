3b68a2af43a3c81491b3df0603084618e3dda05e
junsu <90898067+developHeo515@users.noreply.github.com>
2025-04-14 22:16:51 +0900

    [Bronze IV] Title: Patyki, Time: 172 ms, Memory: 17592 KB -BaekjoonHub

---
+++ 백준/Bronze/8723. Patyki/Patyki.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class Main{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        if(a*a == (b*b + c*c) || b*b == (a*a + c*c) || c*c == (a*a + b*b)) System.out.println(1);
+        else if ((a == b) && (b == c)) System.out.println(2);
+        else System.out.println(0);
+    }
+}
