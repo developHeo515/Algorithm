fb014e94b51af27a3bcd620e7650f39ccf313018
junsu <90898067+developHeo515@users.noreply.github.com>
2025-06-06 22:57:51 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 9.03 ms, Memory: 54.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -0,0 +1,49 @@
+import java.io.*;
+import java.util.*;
+
+class Solution {
+    static int[] dx = {0, 1, 0, -1}; //우 하 좌 상
+    static int[] dy = {1, 0, -1, 0};
+    static int[][] board;
+    public int solution(int[][] maps) {
+        int answer = 0;
+        int n = maps.length;
+        int m = maps[0].length;
+
+        board = new int[n][m];
+
+        answer = bfs(maps, n, m);
+
+        return answer;
+    }
+
+    static int bfs(int[][] maps , int n, int m) {
+        Queue<int[]> q = new LinkedList<>();
+        q.add(new int[] {0, 0});
+        board[0][0] = 1;
+
+        while(!q.isEmpty()) {
+            int[] cur = q.poll();
+            int x = cur[0];
+            int y = cur[1];
+
+            for(int dir = 0; dir < 4; dir++) {
+                int nx = x + dx[dir];
+                int ny = y + dy[dir];
+
+                if(nx < 0 || ny < 0 || nx >= n || ny >= m) continue;
+                if(maps[nx][ny] == 0) continue;
+                if(board[nx][ny] == 0) {
+                    board[nx][ny] = board[x][y] + 1;
+                    q.add(new int[] {nx, ny});
+                    if(nx == n-1 && ny == m-1) {
+                        return board[nx][ny];
+                    }
+                }
+            }
+        }
+
+        return -1;
+    }
+
+}
