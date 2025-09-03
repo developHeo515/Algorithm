595668c121f5fce3db0cc0ba49cff5e7b4cda558
junsu <90898067+developHeo515@users.noreply.github.com>
2025-09-03 22:15:46 +0900

    [Silver V] Title: 공포의 면담실, Time: 128 ms, Memory: 14788 KB, Score: 36 point -BaekjoonHub

---
+++ 백준/Silver/30088. 공포의 면담실/공포의 면담실.java
@@ -0,0 +1,44 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+
+        for(int i = 0; i < N; i++){
+            st = new StringTokenizer(br.readLine());
+            int num = Integer.parseInt(st.nextToken());
+            int cnt = 0;
+
+            for(int j = 0; j < num; j++){
+                int time = Integer.parseInt(st.nextToken());
+                cnt += time;
+            }
+            arr[i] = cnt;
+        }
+
+        Arrays.sort(arr);
+        for(int i = 0; i < N; i++){
+            if(i == 0){
+                continue;
+            }
+            else {
+                arr[i] = arr[i] + arr[i-1];
+            }
+        }
+
+        int res = 0;
+        for(int i = 0; i < N; i++){
+           res += arr[i];
+        }
+        bw.write(res + "\n");
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+}
