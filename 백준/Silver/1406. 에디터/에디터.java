ef4075488ce256f3f8945f9b7f639294dab826dc
junsu <90898067+developHeo515@users.noreply.github.com>
2025-11-24 12:43:29 +0900

    [Silver II] Title: 에디터, Time: 536 ms, Memory: 116836 KB -BaekjoonHub

--- 백준/Silver/1406. 에디터/에디터.java
+++ 백준/Silver/1406. 에디터/에디터.java
@@ -2,51 +2,64 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st;
 
-        List<Character> list = new LinkedList<>();
         String str = br.readLine();
         int n = Integer.parseInt(br.readLine());
+        Deque<Character> leftStack = new ArrayDeque<>();
+        Deque<Character> rightStack = new ArrayDeque<>();
 
-        for (int i = 0; i < str.length(); i++) {
-            list.add(str.charAt(i));
+        for(int i = 0; i < str.length(); i++){
+            leftStack.push(str.charAt(i));
         }
 
-        ListIterator<Character> li = list.listIterator(list.size());
-
-        for (int i = 0; i < n; i++) {
+        for(int i = 0; i < n; i++){
             st = new StringTokenizer(br.readLine());
-            String edit = st.nextToken();
+            char ch = st.nextToken().charAt(0);
 
-            if (edit.equals("P")) {
-                char ch = st.nextToken().charAt(0);
-                li.add(ch);
-            } else if (edit.equals("L")) {
-                if (li.hasPrevious()) {
-                    li.previous();
-                }
-            } else if (edit.equals("D")) {
-                if (li.hasNext()) {
-                    li.next();
-                }
-            } else if (edit.equals("B")) {
-                if (li.hasPrevious()) {
-                    li.previous();
-                    li.remove();
-                }
+            switch(ch) {
+                case 'P' :
+                    char c = st.nextToken().charAt(0);
+                    leftStack.push(c);
+                    break;
+
+                case 'L' :
+                    if(!leftStack.isEmpty()){
+                        char move = leftStack.pop();
+                        rightStack.push(move);
+                    }
+                    break;
+
+                case 'D' :
+                    if(!rightStack.isEmpty()){
+                        char move = rightStack.pop();
+                        leftStack.push(move);
+                    }
+                    break;
+
+                case 'B' :
+                    if(!leftStack.isEmpty()){
+                        leftStack.pop();
+                    }
+                    break;
+
+                default :
+                    break;
             }
         }
 
-        while (li.hasPrevious()) {
-            li.previous();
+        //출력을 위한 이동
+        while(!leftStack.isEmpty()){
+            char move = leftStack.pop();
+            rightStack.push(move);
         }
-
-        while (li.hasNext()) {
-            bw.write(li.next());
+        //출력
+        while(!rightStack.isEmpty()){
+            char print = rightStack.pop();
+            bw.write(print);
         }
 
         bw.flush();
