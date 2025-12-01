15db3460aafc2c787fc4ab9c4f4fa44d08d3a3b5
junsu <90898067+developHeo515@users.noreply.github.com>
2025-12-01 13:38:15 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 112 ms, Memory: 14384 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
@@ -1,53 +1,60 @@
-// 백준 2309번 읽곱 난쟁이 브론즈1
 import java.io.*;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		int totalSave = 0;
-		int total = 0;
-		int [] dwarf = new int[9];
-		
-		for(int i = 0; i < 9; i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-//			System.out.println(Integer.parseInt(st.nextToken()));
-			dwarf[i] = Integer.parseInt(st.nextToken());
-			total += dwarf[i];
-			totalSave = total; //세이브본 저장
-//			System.out.println(total);
-		}
-		Arrays.sort(dwarf);
-		
-		int spy1 = 0;
-		int spy2 = 0;
-		
-		loop:
-		for(int i = 0; i < 9; i++) {
-			for(int j = 0; j < 9; j++) {
-				if(i == j)
-					continue;
-				
-				total -= (dwarf[i] + dwarf[j]);
-				if(total == 100) {
-//					System.out.println(dwarf[i] + ", " +dwarf[j]);
-//					spy1 = dwarf[i];
-//					spy2 = dwarf[j];
-					spy1 = i;
-					spy2 = j;
-					break loop;
-				}
-				total = totalSave;
-				
-			}
-		}
-		
-		for(int i = 0; i < 9; i++) {
-			if(i != spy1 && i != spy2)
-				System.out.println(dwarf[i]);
-		}
-	
-	}
+    static boolean finish = false;
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int[] arr = new int[9];
+        boolean[] tf = new boolean[9];
+
+        for(int i = 0; i < 9; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        dfs(0, arr, tf);
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+    static void dfs(int cnt, int[] arr, boolean[] tf){
+        if(finish) return;
+        if(cnt == 7){
+            //100되는지 탐색
+            int sum = 0;
+            for(int k = 0; k < 9; k++){
+                if(tf[k]){
+                    sum += arr[k];
+                }
+            }
+            if(sum == 100){
+                int[] result = new int[7];
+                int idx = 0;
+                for(int k = 0; k < 9; k++){
+                    if(tf[k]){
+                        result[idx++] = arr[k];
+                    }
+                }
+                Arrays.sort(result);
+
+                for(int r = 0; r < 7; r++){
+                    System.out.println(result[r]);
+                }
+                finish = true;
+                return;
+            }
+            return;
+        }
+
+        for(int i = 0; i < 9; i++){
+            if(!tf[i]){ //중복안되는 난쟁이일때
+                tf[i] = true;
+                dfs(cnt+1, arr, tf);
+                tf[i] = false;
+            }
+        }
+    }
 }
