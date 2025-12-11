38df510a41c1b6cef7ecd9476a5acd27ed586f2d
junsu <90898067+developHeo515@users.noreply.github.com>
2025-12-11 15:08:18 +0900

    [level 2] Title: 기능개발, Time: 0.25 ms, Memory: 73.8 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -3,35 +3,40 @@ import java.util.*;
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
         int[] answer = {};
-        List<Integer> list = new LinkedList<>();
-        int day = 0;
-        int idx = 0;
-        int cnt = 0;
+        Deque<Integer> q = new ArrayDeque<>();
+        List<Integer> list = new ArrayList<>();
 
-        while(speeds.length > idx){
-            if(progresses[idx] + (day * speeds[idx]) >= 100){
-                cnt++;
-                idx++;
+
+        for(int i = 0; i < speeds.length; i++){
+            int remain = (100 - progresses[i]) / speeds[i];
+            int quot = (100 - progresses[i]) % speeds[i];
+            if(quot != 0){
+                remain++;
+            }
+
+            //앞에 애들 먼저 배포해야할때
+            if(!q.isEmpty() && q.peek() < remain){
+                list.add(q.size());
+                q.clear();
+
+                //앞에 애들 다 배포하고 새로 저장
+                q.add(remain);
             }
             else {
-                if(cnt != 0){
-                    list.add(cnt);
-                    cnt = 0;
-                }
-                day++;
+                q.add(remain);
             }
         }
 
-        if(cnt != 0){
-            list.add(cnt);
+        if(!q.isEmpty()){
+            list.add(q.size());
         }
 
         answer = new int[list.size()];
         for(int i = 0; i < list.size(); i++){
             answer[i] = list.get(i);
-            // System.out.print(answer[i] + " ");
         }
 
+//        System.out.println(Arrays.toString(answer));
         return answer;
     }
 }
