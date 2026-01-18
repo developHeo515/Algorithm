7c4ffb92d115144e2f09e52343adc9627e02ea70
junsu <90898067+developHeo515@users.noreply.github.com>
2026-01-18 17:02:31 +0900

    [level 2] Title: 카펫, Time: 346.18 ms, Memory: 80.8 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -1,25 +1,22 @@
 class Solution {
     public int[] solution(int brown, int yellow) {
         int[] answer = new int[2];
-
         int total = brown + yellow;
 
-        for (int i = 3; i <= total; i++) {
-            int col = i;
-            int row = total / col;
-
-            if (row < 3) {
-                continue;
-            }
-
-            if (row >= col) {
-                if ((row - 2) * (col - 2) == yellow) {
-                    answer[0] = row;
-                    answer[1] = col;
-                    break;
+        for(int col = 3; col <= total / 3; col++){
+            //가로(row) >= 세로(col) 이라서 이렇게 함
+            for(int row = col; row <= total / 3; row++){
+                if(row * col != total) continue;
+                else {
+                    if((row - 2) * (col - 2) == yellow){
+                        answer[0] = row;
+                        answer[1] = col;
+                        return answer;
+                    }
                 }
             }
         }
+        
         return answer;
     }
 }
