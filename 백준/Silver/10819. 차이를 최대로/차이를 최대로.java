02ebcb62abab21b479b54f0099aa4f41e9bed471
junsu <90898067+developHeo515@users.noreply.github.com>
2026-01-21 21:37:42 +0900

    [Silver II] Title: 차이를 최대로, Time: 120 ms, Memory: 16504 KB -BaekjoonHub

--- 백준/Silver/10819. 차이를 최대로/차이를 최대로.java
+++ 백준/Silver/10819. 차이를 최대로/차이를 최대로.java
@@ -1,63 +1,49 @@
-//BOJ10819 차이를 최대로, 실버2, 1시간 20분
-//백트래킹 문제, 백트래킹 너무 어렵다..
 import java.io.*;
 import java.util.*;
 
 public class Main {
-	static int N;
-	static int[] arr;
-	static boolean[] selected;
-	static int max = 0;
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringTokenizer st;
-		
-		N = Integer.parseInt(br.readLine());
-		arr = new int[N];
-		selected = new boolean[N];
-		st = new StringTokenizer(br.readLine());
-		for(int i = 0; i < N; i++) {
-			arr[i] = Integer.parseInt(st.nextToken());
-		}
-		
-		back(0, -1, 0);
-		
-		bw.write(max + "\n");
-		bw.flush();
-		bw.close();
-		br.close();
-	}
-	static void back(int depth, int prevIdx, int sum) {
-		if(depth == N) {
-			//max 랑 합비교해서 max 값 정하기
-//			System.out.println(sum);
-			max = Math.max(max, sum);
-			return;
-		}
-		
-		for(int i = 0; i < N; i++) {
-			if(!selected[i]) {
-				selected[i] = true;
-				
-				if(prevIdx == -1) {
-					back(depth + 1, i, sum);
-				} 
-				else {
-				    sum += Math.abs(arr[prevIdx] - arr[i]);  //여기서 애먹음;
-				    back(depth + 1, i, sum);
-				    sum -= Math.abs(arr[prevIdx] - arr[i]);
-				}
-				//더 적합한 방안
-//				else {
-//				    int diff = Math.abs(arr[prevIdx] - arr[i]);
-//				    back(depth + 1, i, sum + diff);
-//				}
+    static int max;
+    static int N;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
 
+        N = Integer.parseInt(br.readLine());
+        int[] A = new int[N];
 
-				
-				selected[i] = false;;
-			}
-		}
-	}
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < N; i++){
+            A[i] = Integer.parseInt(st.nextToken());
+        }
+
+        max = 0;
+        dfs(0, new boolean[N], new int[N], A);
+
+        bw.write(max + "\n");
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+    static void dfs(int depth, boolean[] visit, int[] select, int[] A){
+        if(depth == N){
+            //N개의 순서가 정해졌으니 계산시작
+            int sum = 0;
+            for(int i = 0; i < N-1; i++){
+                sum += Math.abs(select[i] - select[i+1]);
+            }
+            max = Math.max(max, sum);
+            return;
+        }
+
+        for(int i = 0; i < N; i++){
+            if(!visit[i]){
+                visit[i] = true;
+                select[depth] = A[i];
+                dfs(depth + 1, visit, select, A);
+                visit[i] = false;
+            }
+        }
+    }
 }
