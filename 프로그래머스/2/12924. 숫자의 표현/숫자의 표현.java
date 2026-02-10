e3bababc4b0a880ab5d511440bcf4c1657b18f42
junsu <90898067+developHeo515@users.noreply.github.com>
2026-02-11 03:56:00 +0900

    [level 2] Title: 숫자의 표현, Time: 0.45 ms, Memory: 54.1 MB -BaekjoonHub

--- 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.java
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.java
@@ -1,18 +1,20 @@
 class Solution {
     public int solution(int n) {
         int answer = 0;
+        int start = 1, end = 1;
+        int sum = 1;
 
-        for(int i = 1; i <= n; i++){
-            int num = 0;
-            for(int j = i; j <= n; j++){
-                num += j;
-                if(num == n){
-                    answer++;
-                    break;
-                }
-                else if(num > n){
-                    break;
-                }
+        while (start <= n) {
+            if (sum < n) {
+                end++;
+                sum += end;
+            } else if (sum > n) {
+                sum -= start;
+                start++;
+            } else { // sum == n
+                answer++;
+                sum -= start;
+                start++;
             }
         }
 
