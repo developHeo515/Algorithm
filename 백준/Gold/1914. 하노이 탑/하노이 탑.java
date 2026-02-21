b9c5a662c53c2a166d26606903a28c22ff570426
junsu <90898067+developHeo515@users.noreply.github.com>
2026-02-21 16:45:42 +0900

    [Gold V] Title: 하노이 탑, Time: 376 ms, Memory: 52580 KB -BaekjoonHub

---
+++ 백준/Gold/1914. 하노이 탑/하노이 탑.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.math.BigInteger;
+import java.util.*;
+
+public class Main {
+    static int cnt = 0;
+    static BufferedWriter bw;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+
+        System.out.println(BigInteger.TWO.pow(N).subtract(BigInteger.ONE));
+
+        if(N <= 20){
+            hanoi(N, 1, 2, 3);
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+    static void hanoi(int N, int A, int B, int C) throws IOException {
+        if(N == 1){
+            bw.write(A + " " + C + "\n");
+            return;
+        }
+
+        hanoi(N-1, A, C, B);
+        bw.write(A + " " + C + "\n");
+        hanoi(N-1, B, A, C);
+    }
+}
