60ad101c6a203f4262ffe3510cac8a9694daa645
junsu <90898067+developHeo515@users.noreply.github.com>
2026-02-25 04:28:20 +0900

    [level 2] Title: 예상 대진표, Time: 0.04 ms, Memory: 89.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
@@ -0,0 +1,17 @@
+class Solution
+{
+    public int solution(int n, int a, int b)
+    {
+        int answer = 0;
+        int round = 1;
+        a--;
+        b--;
+        while(a / 2 != b / 2){
+            a /= 2;
+            b /= 2;
+            round++;
+        }
+
+        return answer = round;
+    }
+}
