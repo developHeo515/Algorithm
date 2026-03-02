54e4233d3434f2b53be3c0d3c25f2d8033f1ab64
junsu <90898067+developHeo515@users.noreply.github.com>
2026-03-02 18:18:53 +0900

    [level 3] Title: 네트워크, Time: 1.10 ms, Memory: 85 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/README.md
+++ 프로그래머스/3/43162. 네트워크/README.md
@@ -1,10 +1,10 @@
 # [level 3] 네트워크 - 43162 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43162?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43162) 
 
 ### 성능 요약
 
-메모리: 75.4 MB, 시간: 0.38 ms
+메모리: 85 MB, 시간: 1.10 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 06월 04일 07:48:33
+2026년 03월 02일 18:18:50
 
 ### 문제 설명
 
--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,35 +1,48 @@
-import java.io.*;
 import java.util.*;
 class Solution {
+    static List<List<Integer>> graph;
     static boolean[] visit;
     public int solution(int n, int[][] computers) {
         int answer = 0;
+        graph = new ArrayList<>();
+
+        for(int i = 0; i < n; i++){
+            graph.add(new ArrayList<>());
+        }
+
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                if(i == j) continue;
+                if(computers[i][j] == 1){
+                    graph.get(i).add(j);
+                }
+            }
+        }
+
+        int cnt = 0;
         visit = new boolean[n];
-
-        for(int i = 0; i < n; i++) {
-            if(!visit[i]) {
-                func(n, computers, i);
-                answer++;
+        for(int i = 0; i < n; i++){
+            if(!visit[i]){
+                cnt++;
+                bfs(i);
             }
         }
-
-
-        return answer;
+        return answer = cnt;
     }
+    static void bfs(int n){
+        Queue<Integer> q = new ArrayDeque<>();
+        visit[n] = true;
+        q.add(n);
 
-    static void func(int n, int[][] computers, int vertex) {
-        visit[vertex] = true;
+        while(!q.isEmpty()){
+            int cur = q.poll();
 
-        for(int i = 0; i < n; i++) {
-            if(i == vertex) continue; 
-            if(visit[i]) continue;
-
-            if(computers[vertex][i] == 1) {
-                visit[i] = true;
-                func(n, computers, i);
+            for(int next : graph.get(cur)){
+                if(!visit[next]){
+                    visit[next] = true;
+                    q.add(next);
+                }
             }
         }
-
-        return;
     }
 }
