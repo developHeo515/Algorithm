5812548d5edf9915f165b2fa7707e2cd8a233180
junsu <90898067+developHeo515@users.noreply.github.com>
2026-03-09 22:34:32 +0900

    [Silver I] Title: 숨바꼭질, Time: 160 ms, Memory: 16944 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -1,56 +1,64 @@
-//BOJ1697 숨바꼭질, 실버1
 import java.io.*;
 import java.util.*;
 
 public class Main {
-	static int N, K, time;
-	static int[] point, parent;
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		N = Integer.parseInt(st.nextToken());
-		K = Integer.parseInt(st.nextToken());
-		time = 0;
-		point = new int[100001];
-		parent = new int[100001];
-		
-		time = bfs();
-	
-		bw.write(time + "\n");
-		bw.flush();
-		bw.close();
-		br.close();
-	}
-	static int bfs() {
-		if(N == K) return 0;
-		Queue<Integer> q = new LinkedList<>();
-		q.add(N);
-		point[N] = 1; //구분을 위해 해둠
-		
-		while(!q.isEmpty()) {
-			int cur = q.poll();
-			int next;
-			
-			for(int i = 0; i < 3; i++) {
-				if(i == 0) next = cur - 1;
-				else if(i == 1) next = cur + 1;
-				else next = cur * 2;
-				
-				if(next < 0 || next > 100000) continue;
-				if(next == K) {
-					return point[cur];
-				}else if(point[next] != 0) { //방문한적있을때
-					continue;
-				}else if(point[next] == 0) {
-					point[next] = point[cur] + 1;
-					q.add(next);
-				}
-			}
-			
-		}
-		
-		return -1;
-	}
+    static int N;
+    static int K;
+    static int time;
+    static boolean[] visited;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+
+        time = 0;
+        visited = new boolean[100000+1];
+        bfs();
+
+        bw.write(time + "\n");
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+    static void bfs(){
+        if(N == K) return;
+
+        Queue<Integer> q = new ArrayDeque<>();
+        visited[N] = true;
+        q.offer(N);
+        boolean flag = false;
+
+        while(!q.isEmpty()){
+            int qsize = q.size();
+
+            while(qsize-- != 0){
+                int cur = q.poll();
+//                System.out.print(cur + " ");
+                if(cur == K) {
+                    flag = true;
+                    break;
+                }
+
+
+                if(cur + 1 <= 100000 && !visited[cur + 1]) {
+                    visited[cur+1] = true;
+                    q.offer(cur + 1);
+                }
+                if(cur - 1 >= 0 && !visited[cur - 1]) {
+                    visited[cur-1] = true;
+                    q.offer(cur - 1);
+                }
+                if(cur * 2 <= 100000 && !visited[cur * 2]) {
+                    visited[cur*2] = true;
+                    q.offer(cur * 2);
+                }
+            }
+            if(flag) break;
+            
+            time++;
+        }
+    }
 }
