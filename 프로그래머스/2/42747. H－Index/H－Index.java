c0b8fe8a424ea1371c45c4d16f51bcb3ae6059d8
junsu <90898067+developHeo515@users.noreply.github.com>
2026-03-25 21:53:21 +0900

    [level 2] Title: H-Index, Time: 28.01 ms, Memory: 85.2 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.java
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -3,27 +3,13 @@ import java.util.Arrays;
 class Solution {
     public int solution(int[] citations) {
         int answer = 0;
-
         Arrays.sort(citations);
-        for(int h = 1; h < citations[citations.length-1]; h++){
-            int cnt = 0;
+        for(int i=0; i<citations.length; i++){
+            int smaller = Math.min(citations[i], citations.length-i);
 
-            //1부터 논문 최대 인용 수
-            for(int j = 0; j < citations.length; j++){
-                // h번 이상 인용된 논문
-                if(h <= citations[j]){
-                    cnt++;
-                }
-
-//                System.out.println("인용수(h): " + h + " - cita : " + citations[j] + "  cnt: " + cnt);
-                if(cnt >= h) {
-//                    System.out.println(h + " - " + cnt + "======");
-                    answer = h;
-                    break;
-                }
-            }
+            System.out.println(smaller + " - " + answer);
+            answer = Math.max(answer, smaller);
         }
-
         return answer;
     }
 }
