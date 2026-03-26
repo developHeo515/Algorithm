a15a6eb0eed2a38e7726a1b221e72e541f6d6cf3
junsu <90898067+developHeo515@users.noreply.github.com>
2026-03-27 00:39:26 +0900

    [level 2] Title: 방문 길이, Time: 0.25 ms, Memory: 81.4 MB -BaekjoonHub

--- 프로그래머스/2/49994. 방문 길이/방문 길이.java
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.java
@@ -1,85 +1,66 @@
 class Solution {
+    static int cnt = 0;
+    // 초기위치 설정
+    static int cx = 5;
+    static int cy = 5;
+    static int nx = 0;
+    static int ny = 0;
     public int solution(String dirs) {
         int answer = 0;
 
         // 각 위치에서 네 방향을 지나갔는지 체크하기 위해 3차원배열 사용
         // 우 하 좌 상 = 0 1 2 3
         boolean[][][] visit = new boolean[11][11][4];
-        // 초기위치 설정
-        int cx = 5;
-        int cy = 5;
-        int nx = 0;
-        int ny = 0;
 
         for(int i = 0; i < dirs.length(); i++){
             char ch = dirs.charAt(i);
+            int curDir = 0;
+            int nextDir = 0;
 
             if(ch == 'U'){
                 nx = cx - 1;
                 ny = cy;
-
-                // 맵 크기 벗어나는지 확인
-                if(nx < 0 || ny < 0 || nx >= 11 || ny >= 11) continue;
-                // 현재 위치에서 위로 갈때와 다음 위치에서 아래로 갈때 길 방문 여부 확인
-                if(!visit[cx][cy][3] && !visit[nx][ny][1]){
-                    visit[cx][cy][3] = true;
-                    visit[nx][ny][1] = true;
-                    cx = nx; // x축만 변경된다.
-                    answer++; // 최초의 길이라 거리추가
-                }
-                else {
-                    cx = nx;
-                }
+                curDir = 3;
+                nextDir = 1;
             }
             else if(ch == 'D'){
                 nx = cx + 1;
                 ny = cy;
-
-                if(nx < 0 || ny < 0 || nx >= 11 || ny >= 11) continue;
-                if(!visit[cx][cy][1] && !visit[nx][ny][3]){
-                    visit[cx][cy][1] = true;
-                    visit[nx][ny][3] = true;
-                    cx = nx;
-                    answer++;
-                }
-                else {
-                    cx = nx;
-                }
+                curDir = 1;
+                nextDir = 3;
             }
             else if(ch == 'R'){
                 nx = cx;
                 ny = cy + 1;
-
-                if(nx < 0 || ny < 0 || nx >= 11 || ny >= 11) continue;
-                if(!visit[cx][cy][0] && !visit[nx][ny][2]){
-                    visit[cx][cy][0] = true;
-                    visit[nx][ny][2] = true;
-                    cy = ny;
-                    answer++;
-                }
-                else {
-                    cy = ny;
-                }
+                curDir = 0;
+                nextDir = 2;
             }
             else if(ch == 'L'){
                 nx = cx;
                 ny = cy - 1;
-
-                if(nx < 0 || ny < 0 || nx >= 11 || ny >= 11) continue;
-                if(!visit[cx][cy][2] && !visit[nx][ny][0]){
-                    visit[cx][cy][2] = true;
-                    visit[nx][ny][0] = true;
-                    cy = ny;
-                    answer++;
-                }
-                else {
-                    cy = ny;
-                }
+                curDir = 2;
+                nextDir = 0;
             }
 
-
+            // 맵 크기 벗어나는지 확인
+            if(nx < 0 || ny < 0 || nx >= 11 || ny >= 11) continue;
+            func(visit, curDir, nextDir);
         }
 
-        return answer;
+        return answer = cnt;
+    }
+    static void func(boolean[][][] visit, int cur, int next){
+        // 현재 위치에서 위로 갈때와 다음 위치에서 아래로 갈때 길 방문 여부 확인
+        if(!visit[cx][cy][cur] && !visit[nx][ny][next]){
+            visit[cx][cy][cur] = true;
+            visit[nx][ny][next] = true;
+            cx = nx;
+            cy = ny;
+            cnt++; // 최초의 길이라 거리추가
+        }
+        else {
+            cx = nx;
+            cy = ny;
+        }
     }
 }
