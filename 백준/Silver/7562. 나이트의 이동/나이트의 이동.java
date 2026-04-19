9aa686d654715e93adcf9ff53950a3ee17bcd371
junsu <90898067+developHeo515@users.noreply.github.com>
2026-04-19 18:55:25 +0900

    [Silver I] Title: 나이트의 이동, Time: 324 ms, Memory: 55076 KB -BaekjoonHub

--- 백준/Silver/7562. 나이트의 이동/나이트의 이동.java
+++ 백준/Silver/7562. 나이트의 이동/나이트의 이동.java
@@ -1,74 +1,63 @@
-//BOJ7562 나이트의 이동, 실1
-//BFS 문제
 import java.io.*;
 import java.util.*;
 
 public class Main {
-	static int[] dx = {-2, -1, 1, 2, 2, 1, -1, -2}; // 나이트 8방향
-	static int[] dy = {1, 2, 2, 1, -1, -2, -2, -1}; //
-	static int L;
-	static int[][] chess;
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringTokenizer st;
-		
-		int T = Integer.parseInt(br.readLine());
-		
-		for(int tc = 1; tc <= T; tc++) {
-			L = Integer.parseInt(br.readLine());
-			chess = new int[L][L];
-			
-			st = new StringTokenizer(br.readLine());
-			int startX = Integer.parseInt(st.nextToken());
-			int startY = Integer.parseInt(st.nextToken());
-			
-			st = new StringTokenizer(br.readLine());
-			int endX = Integer.parseInt(st.nextToken());
-			int endY = Integer.parseInt(st.nextToken());
-			
-//			System.out.println(startX + " " +  startY + " " + endX + " " + endY);
-			int res = BFS(startX, startY, endX, endY);
-			bw.write(res + "\n");
-		}
-		
-		bw.flush();
-		bw.close();
-		br.close();
-		
-	}
-	static int BFS(int startX, int startY, int endX, int endY) {
-		int cnt = 0 ;
-		Queue<int []> q = new LinkedList<>();
-		q.add(new int[] {startX, startY});
-		boolean[][] visit = new boolean[L][L];
-		visit[startX][startY] = true;
-		
-		while(!q.isEmpty()) {
-			int qsize = q.size();
-			
-			while(qsize-- != 0) {
-				int[] cur = q.poll();
-				int x = cur[0];
-				int y = cur[1];
-				
-				if(x == endX && y == endY) {
-					return cnt;
-				}
-				
-				for(int dic = 0; dic < 8; dic++) {
-					int nx = x + dx[dic];
-					int ny = y + dy[dic];
-					if(nx < 0 || ny < 0 || nx >= L || ny >= L) continue;
-					if(visit[nx][ny]) continue;
-					
-					visit[nx][ny] = true;
-					q.add(new int[] {nx, ny});
-				}
-			}
-			
-			cnt++;
-		}
-		return -1;
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+
+        int T = Integer.parseInt(br.readLine());
+
+        for(int tc = 0; tc < T; tc++){
+            int l = Integer.parseInt(br.readLine());
+            int[] dx = {-2, -1, 1, 2, 2, 1, -1, -2}; //1시 방향이 스타트
+            int[] dy = {1, 2, 2, 1, -1, -2, -2, -1};
+            int[] present = new int[2];
+            int[] goal = new int[2];
+            int cnt = 0;
+
+            st = new StringTokenizer(br.readLine());
+            present[0] = Integer.parseInt(st.nextToken());
+            present[1] = Integer.parseInt(st.nextToken());
+
+            st = new StringTokenizer(br.readLine());
+            goal[0] = Integer.parseInt(st.nextToken());
+            goal[1] = Integer.parseInt(st.nextToken());
+
+            //bfs 실행
+            Queue<int[]> q = new ArrayDeque<>();
+            q.offer(new int[] {present[0], present[1], 0});
+            boolean[][] visit = new boolean[l][l];
+            visit[present[0]][present[1]] = true;
+
+            while(!q.isEmpty()){
+                int[] cur = q.poll();
+                int cx = cur[0];
+                int cy = cur[1];
+                int ct = cur[2];
+
+                if(cx == goal[0] && cy == goal[1]){
+                    cnt = ct;
+                    break;
+                }
+
+                for(int dir = 0; dir < 8; dir++){
+                    int nx = cx + dx[dir];
+                    int ny = cy + dy[dir];
+                    if(nx < 0 || ny < 0 || nx >= l || ny >= l) continue;
+                    if(!visit[nx][ny]) {
+                        q.offer(new int[] {nx, ny, ct+1});
+                        visit[nx][ny] = true;
+                    }
+                }
+            }
+
+            bw.write(cnt + "\n");
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
 }
